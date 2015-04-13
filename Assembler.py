c2e208a17f4f7bb4c5893249b978f1dca3bfdc72
James Murphy <jmurphy4@mail.umw.edu>
2015-04-13 16:14:09 -0400

    Added (stupid) compute logic

--- Assembler.py
+++ Assembler.py
@@ -91,7 +91,42 @@ def get_dest_bits(string):
 	return a + d + m
 
 def get_comp_bits(string):
-	pass
+	# forgive me, but this code is stupid
+	# it works, but it's not smart and would be totally impractical
+	# on literally any other processor; I just can't think of a smarter
+	# way to do it
+	
+	mnemonics = {
+		# A-operations first
+		"0" : "0101010", "1" : "0111111",
+		"-1" : "0111010", "D" : "0001100",
+		"A" : "0110000", "!D" : "0001101",
+		"!A" : "0110001", "-D" : "0001111",
+		"-A" : "0110011", "D+1" : "0011111",
+		"A+1" : "0110111", "D-1" : "0001110",
+		"A-1" : "0110010", "D+A" : "0000010",
+		"A+D" : "0000010", # forgive A+D since + is commutative
+		"D-A" : "0010011", "A-D" : "0000111",
+		"D&A" : "0000000", "D|A" : "0010101",
+		# forgive inverted A&D and A|D, since & and | are commutative
+		"A&D" : "0000000", "A|D" : "0010101",
+		
+		# M-operations
+		"M" : "1110000", "!M" : "1110001",
+		"-M" : "1110011", "M+1" : "1110111",
+		"M-1" : "1110010", "D+M" : "1000010",
+		"M+D" : "1000010" # forgive M+D since addition is commutative
+		"D-M" : "1010011", "M-D" : "1000111",
+		"D&M" : "1000000", "D|M" : "1010101",
+		# forgive inverted M&D and M|D since & and | are commutative
+		"M&D" : "1000000", "M|D" : "1010101"
+	}
+	try:
+		comp = mnemonics[string]
+	except:
+		sys.stdout.write("'%s' is an invalid command!\n" %string)
+		sys.exit()
+	return comp
 	
 # command line argument passed is the input file for the Assembler
 try:
@@ -111,6 +146,19 @@ except:
 
 assembly = open(filename, 'r')
 machine = open(outfile, 'w')
+
+for line in assembly:
+	# ignore comments
+	if line.lstrip()[0:2] == "//":
+		continue
+	# ignore empty/whitespace-only lines
+	if line.strip() == "":
+		continue
+	
+	if is_ainstr(line.strip()):
+		machine.write(ainstr(line.strip()))
+	else:
+		machine.write(cinstr(line.strip()))
 
 assembly.close()
 machine.close()
