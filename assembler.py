ad61d4c2df38c6fed802a3ff8d904fd80da9f3b6
James Murphy <jmurphy4@mail.umw.edu>
2017-04-13 23:49:11 -0400

    whoa whoa whoa blast from the past

--- assembler.py
+++ assembler.py
@@ -3,17 +3,17 @@ from helper import *
 
 # pre-defined addresses to be used/appended to by ainstr()
 addresses = {
-	"R0" : "0",    "SP" : "0",
-	"R1" : "1",    "LCL" : "1",
-	"R2" : "2",    "ARG" : "2",
-	"R3" : "3",    "THIS" : "3",
-	"R4" : "4",    "THAT" : "4",
-	"R5" : "5",    "R6" : "6",
-	"R7" : "7",    "R8" : "8",
-	"R9" : "9",    "R10" : "10",
-	"R11" : "11",  "R12" : "12",
-	"R13" : "13",  "R14" : "14",
-	"R15" : "15",  "SCREEN" : "16384",
+	"R0"  : "0",    "SP"     : "0",
+	"R1"  : "1",    "LCL"    : "1",
+	"R2"  : "2",    "ARG"    : "2",
+	"R3"  : "3",    "THIS"   : "3",
+	"R4"  : "4",    "THAT"   : "4",
+	"R5"  : "5",    "R6"     : "6",
+	"R7"  : "7",    "R8"     : "8",
+	"R9"  : "9",    "R10"    : "10",
+	"R11" : "11",   "R12"    : "12",
+	"R13" : "13",   "R14"    : "14",
+	"R15" : "15",   "SCREEN" : "16384",
 	"KBD" : "24576"
 }
 
@@ -32,34 +32,31 @@ assembly = open(filename, 'r')
 # enter labels into dictionary
 romline = 0
 labels = {}
-for line in assembly:
-	# labels mark the following address of ROM, so pre-increment the count,
-	# but only if it's a non-label line of code
-	temp = line.strip()
-	if not is_skippable(temp):
-		romline += 1
-	if temp != "" and temp[0] == "(":
-		labels[prepare_label(temp)] = str(romline)
 
-# close and reopen to start from top of file
-assembly.close()
-assembly = open(filename, 'r')
-machine = open(outfile, 'w')
+with open(filename, 'r') as assembly:
+	for line in assembly:
+		# labels mark the following address of ROM, so pre-increment
+		# the count, but only if it's a non-label line of code
+		temp = line.strip()
+		if not is_skippable(temp):
+			romline += 1
+		if temp != "" and temp[0] == "(":
+			labels[prepare_label(temp)] = str(romline)
 
 # assemble
-for line in assembly:
-	command = line.strip()
-	# ignore comments, whitespace, empty lines, and labels
-	if is_skippable(command):
-		continue
-	if "//" in command:
-		command = pop_inline_comment(command)
-	# write the A instruction as a binary string to the .hack file	
-	if is_ainstr(command):
-		machine.write(ainstr(command, labels, addresses))
-	# write the C instruction as a binary string to the .hack file
-	else:
-		machine.write(cinstr(command))
+with open(filename, 'r') as assembly:
+	with open(outfile, 'w') as machine:
+		for line in assembly:
+			command = line.strip()
+			# ignore comments, whitespace, empty lines, and labels
+			if is_skippable(command):
+				continue
+			if "//" in command:
+				command = pop_inline_comment(command)
+			# write the A instruction as a binary string to the .hack file	
+			if is_ainstr(command):
+				machine.write(ainstr(command, labels, addresses))
+			# write the C instruction as a binary string to the .hack file
+			else:
+				machine.write(cinstr(command))
 
-assembly.close()
-machine.close()
--- helper.py
+++ helper.py
@@ -84,42 +84,42 @@ def cinstr(command):
 def get_jump_bits(string):
 	jumpbits = ""
 	jumps = {
-		"" : "000",    "JGT" : "001",
+		""    : "000", "JGT" : "001",
 		"JEQ" : "010", "JGE" : "011",
 		"JLT" : "100", "JNE" : "101",
 		"JLE" : "110", "JMP" : "111"
 	}
 	try:
 		jumpbits = jumps[string]
-	except:
-		raise Exception("'%s' is an invalid jump target!\n" %string)
+	except KeyError as e:
+		raise Exception("'%s' is an invalid jump target!\n" % string)
 	
 	return jumpbits
 
 # function to parse destination of a command and return 3-bit string
 def get_dest_bits(string):
-	d = "1" if "D" in string else "0"
-	a = "1" if "A" in string else "0"
-	m = "1" if "M" in string else "0"
-	return "%s%s%s" %(a, d, m)
+	a = int("A" in string)
+	d = int("D" in string)
+	m = int("M" in string)
+	return "%d%d%d" % (a, d, m)
 
 # function to parse comp of a command and return a 7-bit string
 def get_comp_bits(string):
 	# small instruction sets ftw, who needs smart programming?
 	mnemonics = {
 		# commutative operations | & + are allowed in both directions
-		"0" : "0101010",   "1" : "0111111",
-		"-1" : "0111010",  "D" : "0001100",
-		"A" : "0110000",   "!D" : "0001101",
-		"!A" : "0110001",  "-D" : "0001111",
-		"-A" : "0110011",  "D+1" : "0011111",
+		"0"   : "0101010", "1"   : "0111111",
+		"-1"  : "0111010", "D"   : "0001100",
+		"A"   : "0110000", "!D"  : "0001101",
+		"!A"  : "0110001", "-D"  : "0001111",
+		"-A"  : "0110011", "D+1" : "0011111",
 		"A+1" : "0110111", "D-1" : "0001110",
 		"A-1" : "0110010", "D+A" : "0000010",
 		"A+D" : "0000010", "D-A" : "0010011",
 		"A-D" : "0000111", "D&A" : "0000000",
 		"D|A" : "0010101", "A&D" : "0000000",
-		"A|D" : "0010101", "M" : "1110000",
-		"!M" : "1110001",  "-M" : "1110011",
+		"A|D" : "0010101", "M"   : "1110000",
+		"!M"  : "1110001",  "-M" : "1110011",
 		"M+1" : "1110111", "M-1" : "1110010",
 		"D+M" : "1000010", "M+D" : "1000010", 
 		"D-M" : "1010011", "M-D" : "1000111",
